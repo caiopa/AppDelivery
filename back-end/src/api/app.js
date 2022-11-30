621a78ce8dcb6eb31b6039ff0001a8e95a3b59d5
henriqueambrosano <henriqueambrosano@hotmail.com>
2022-11-30 20:42:37 -0300

    inserindo cors no app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const errorMiddleware = require('../middlewares/error.middleware');
+const cors = require('cors');
 const loginRouter = require('../routes/login.routes');
 const registerRouter = require('../routes/register.routes')
 const productRouter = require('../routes/customer.routes');
@@ -7,6 +8,7 @@ const productRouter = require('../routes/customer.routes');
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use(loginRouter);
