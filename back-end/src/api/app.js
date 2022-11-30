5267620b7f2b615c2bcce3219e377ffd02a6593d
henriqueambrosano <henriqueambrosano@hotmail.com>
2022-11-30 16:26:16 -0300

    adicionando novo router no app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const errorMiddleware = require('../middlewares/error.middleware');
 const loginRouter = require('../routes/login.routes');
+const registerRouter = require('../routes/register.routes')
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(express.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use(loginRouter);
+app.use(registerRouter);
 app.use(errorMiddleware);
 
 module.exports = app;
