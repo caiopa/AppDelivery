6fd0949510efb8eb133ce3701886d713a21c6623
henriqueambrosano <henriqueambrosano@hotmail.com>
2022-12-05 17:57:14 -0300

    fix: passando id como prop

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -1,36 +1,36 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-function ProductCard({ price, urlImage, name, key }) {
+function ProductCard({ price, urlImage, name, id }) {
   return (
     <div data-testid="">
-      <p data-testid={ `customer_products__element-card-price-${key}` }>
+      <p data-testid={ `customer_products__element-card-price-${id}` }>
         {`R$ ${price}`}
       </p>
       <img
-        data-testid={ `customer_products__img-card-bg-image-${key}` }
+        data-testid={ `customer_products__img-card-bg-image-${id}` }
         src={ urlImage }
         alt={ name }
       />
       <p
-        data-testid={ `customer_products__element-card-title-${key}` }
+        data-testid={ `customer_products__element-card-title-${id}` }
       >
         {name}
       </p>
       <button
         type="button"
-        data-testid={ `customer_products__button-card-rm-item-${key}` }
+        data-testid={ `customer_products__button-card-rm-item-${id}` }
       >
         -
       </button>
       <span
-        data-testid={ `customer_products__input-card-quantity-${key}` }
+        data-testid={ `customer_products__input-card-quantity-${id}` }
       >
         0
       </span>
       <button
         type="button"
-        data-testid={ `customer_products__button-card-add-item-${key}` }
+        data-testid={ `customer_products__button-card-add-item-${id}` }
       >
         +
       </button>
@@ -42,6 +42,7 @@ ProductCard.propTypes = {
   name: PropTypes.string,
   price: PropTypes.number,
   urlImage: PropTypes.string,
+  id: PropTypes.number,
 }.isRequired;
 
 export default ProductCard;
