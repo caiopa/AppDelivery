1ad7e869943eab9595900fff6058e7cb4eb713f3
henriqueambrosano <henriqueambrosano@hotmail.com>
2022-12-05 19:55:54 -0300

    feat: criando a logica para adicionar e remover itens no carrinho e salvar em localStorage

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -1,7 +1,51 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function ProductCard({ price, urlImage, name, id }) {
+  const [qty, setQty] = useState(0);
+
+  const addToCart = () => {
+    if (localStorage.getItem('carrinho')) {
+      let cart = JSON.parse(localStorage.getItem('carrinho'));
+      const exists = cart.some((prod) => prod.name === name);
+      if (exists) {
+        cart = cart.reduce((acc, curr) => {
+          if (curr.name === name) {
+            curr.qty = qty;
+            return acc;
+          }
+          return acc;
+        }, cart);
+      } else {
+        cart = [
+          ...cart,
+          { name, price, qty },
+        ];
+      }
+      const updatedCart = cart.filter((prod) => prod.qty);
+      localStorage.setItem('carrinho', JSON.stringify(updatedCart));
+    } else {
+      localStorage.setItem('carrinho', JSON.stringify([{ name, price, qty }]));
+    }
+  };
+
+  useEffect(() => {
+    addToCart();
+  }, [qty]);
+
+  const handleChange = (value) => {
+    if (value < 0 || value === '') value = '';
+    setQty(value);
+  };
+
+  const handleClick = (value) => {
+    if (value === '+') {
+      setQty(+qty + 1);
+    } else if (value === '-' && qty > 0) {
+      setQty(+qty - 1);
+    }
+  };
+
   return (
     <div data-testid="">
       <p data-testid={ `customer_products__element-card-price-${id}` }>
@@ -20,17 +64,23 @@ function ProductCard({ price, urlImage, name, id }) {
       <button
         type="button"
         data-testid={ `customer_products__button-card-rm-item-${id}` }
+        onClick={ (e) => handleClick(e.target.value) }
+        value="-"
       >
         -
       </button>
-      <span
-        data-testid={ `customer_products__input-card-quantity-${id}` }
-      >
-        0
-      </span>
+      <input
+        datatestid={ `customer_products__input-card-quantity-${id}` }
+        type="number"
+        placeholder="0"
+        value={ qty }
+        onChange={ (e) => handleChange(e.target.value) }
+      />
       <button
         type="button"
         data-testid={ `customer_products__button-card-add-item-${id}` }
+        onClick={ (e) => handleClick(e.target.value) }
+        value="+"
       >
         +
       </button>
