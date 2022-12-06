8bd543b5b2617f2f11a6aead51a3c29d128f75ff
Samuelr499 <samuelribeiro2@hotmail.com>
2022-12-06 13:54:36 -0300

    feat: CustomerCheckout

---
+++ front-end/src/pages/CustomerCheckout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import TableOrders from '../components/TableOrders';
+
+function CustomerCheckout() {
+  return (
+    <div>
+      <Header />
+      <TableOrders />
+    </div>
+  );
+}
+
+export default CustomerCheckout;
