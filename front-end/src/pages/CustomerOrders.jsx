e037a643db168bdb68e9bd32780af285b49e1c2e
Samuelr499 <samuelribeiro2@hotmail.com>
2022-12-06 13:54:52 -0300

    feat: CustomerOrders

---
+++ front-end/src/pages/CustomerOrders.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import OrdersCard from '../components/OrdersCard';
+
+function CustomerOrders() {
+  return (
+    <div>
+      <Header />
+      <OrdersCard />
+    </div>
+  );
+}
+
+export default CustomerOrders;
