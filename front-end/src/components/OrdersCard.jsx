7ab44572af7eacdf6fc82f956020c9b87806675c
Samuelr499 <samuelribeiro2@hotmail.com>
2022-12-07 12:02:28 -0300

    feat: implementaçào do cart Orders

--- front-end/src/components/OrdersCard.jsx
+++ front-end/src/components/OrdersCard.jsx
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { apiGet } from '../services/requests';
+import getToken from '../utils/getToken';
+import convertDate from '../utils/converteDate';
 
 function OrdersCard() {
+  const history = useHistory();
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    apiGet('/orders', getToken()).then((res) => setOrders(res));
+  }, []);
+
+  console.log(orders);
   return (
     <div>
-      <p data-testid="customer_orders__element-order-id-<id>">
-        pedido 0001
-      </p>
-      <p
-        data-testid="customer_orders__element-delivery-status-<id>"
-      >
-        PENDENTE
-      </p>
-      <p data-testid="customer_orders__element-order-date-<id>">
-        dd-mm-aaaa
-      </p>
-      <p data-testid="customer_orders__element-card-price-<id>">
-        {'R$ 28,46 '}
-      </p>
+      {
+        !orders.length ? 'sem pedido  :('
+          : (
+            orders.map(({ id, status, saleDate, totalPrice }) => (
+              <button
+                key={ id }
+                type="button"
+                onClick={ () => history.push(`/customer/orders/${id}`) }
+              >
+                <div>
+
+                  <p data-testid={ `customer_orders__element-order-id-${id}` }>
+                    {`Pedido ${id}`}
+                  </p>
+                  <p data-testid={ `customer_orders__element-delivery-status-${id}` }>
+                    {status}
+                  </p>
+                  <p data-testid={ `customer_orders__element-order-date-${id}` }>
+                    {convertDate(saleDate)}
+                  </p>
+                  <p data-testid={ `customer_orders__element-card-price-${id}` }>
+                    {`R$ ${totalPrice} `}
+                  </p>
+                </div>
+              </button>
+            ))
+          )
+      }
     </div>
   );
 }
