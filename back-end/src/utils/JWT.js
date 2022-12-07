58a71d52fe874434ee03fcc2b90ce9b0f069d6b1
henriqueambrosano <henriqueambrosano@hotmail.com>
2022-12-07 17:04:00 -0300

    refactor:  corrigindo importacao do SECRET jwt

--- back-end/src/utils/JWT.js
+++ back-end/src/utils/JWT.js
@@ -1,6 +1,6 @@
 const { verify, sign } = require('jsonwebtoken');
-
-const SECRET = process.env.JWT_SECRET;
+const fs = require('fs');
+const SECRET = fs.readFileSync('jwt.evaluation.key', { encoding: 'utf-8' })
 
 const createToken = (payload) => {
   const jwtConfig = {
