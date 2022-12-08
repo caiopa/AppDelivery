b7883458defb66038dacaf93c4d7d3c1cca9f07d
henriqueambrosano <henriqueambrosano@hotmail.com>
2022-12-08 01:42:57 -0300

    adicionando rota de getUsers

--- back-end/src/routes/routes.js
+++ back-end/src/routes/routes.js
@@ -8,15 +8,15 @@ const router = express.Router();
 const productController = new ProductController();
 const saleController = new SaleController();
 const userController = new UserController();
-
-router.post('/login', (req, res, next) => userController.login(req, res, next));
-router.post('/register', (req, res, next) => userController.register(req, res, next));
-router.post('/admregister', JWT, (req, res, next) => userController.admRegister(req, res, next));
 router.get('/sellers', (req, res, next) => userController.getSellers(req, res, next));
 router.get('/products', (req, res, next) => productController.getAll(req, res, next));
 router.get('/orders', JWT, (req, res, next) => saleController.getAll(req, res, next));
-router.post('/orders', JWT, (req, res, next) => saleController.create(req, res, next));
+router.get('/users', JWT, (req, res, next) => userController.getUsers(req, res, next));
 router.get('/orders/:id', JWT, (req, res, next) => saleController.getOne(req, res, next));
+router.post('/admregister', JWT, (req, res, next) => userController.admRegister(req, res, next));
+router.post('/login', (req, res, next) => userController.login(req, res, next));
+router.post('/register', (req, res, next) => userController.register(req, res, next));
+router.post('/orders', JWT, (req, res, next) => saleController.create(req, res, next));
 router.put('/orders/:id', JWT, (req, res, next) => saleController.update(req, res, next));
 
 module.exports = router;
