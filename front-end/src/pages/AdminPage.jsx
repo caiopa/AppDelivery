049c7874f970ab69f0d47a200ab8a8b9c0ae16a3
henriqueambrosano <henriqueambrosano@hotmail.com>
2022-12-08 01:23:29 -0300

    feat: adicionando formulario de registro na pagina de admin

--- front-end/src/pages/AdminPage.jsx
+++ front-end/src/pages/AdminPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import AdminRegister from '../components/AdminRegister';
 import Header from '../components/Header';
 
 function AdminPage() {
   return (
     <div>
       <Header />
+      <AdminRegister />
+      <div>
+        <h2>Cadastrar novo usuário</h2>
+      </div>
     </div>
   );
 }
