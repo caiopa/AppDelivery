4de7a4dd885b5f81c8a5e903c669e20e0f5009f9
henriqueambrosano <henriqueambrosano@hotmail.com>
2022-12-08 01:50:38 -0300

    feat: adicionando funcao de getUsers no controller de users

--- back-end/src/controllers/user.controller.js
+++ back-end/src/controllers/user.controller.js
@@ -44,6 +44,15 @@ class UserController {
       next(e);
     }
   }
+
+  async getUsers(_req, res, next) {
+    try {
+      const users = await this.service.getUsers();
+      return res.status(200).json(users);
+    } catch (e) {
+      next(e);
+    }
+  }
 }
 
 module.exports = UserController;
